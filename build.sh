364f14434b0dd4b91256380346f2e83196385b54
Alper Kanat <me@alperkan.at>
2024-08-05 01:54:26 +0300

    Removes system76 packages

--- build.sh
+++ build.sh
@@ -3,10 +3,7 @@
 set -ouex pipefail
 
 rpm-ostree install \
-    system76-driver \
-    system76-firmware \
     system76-keyboard-configurator \
-    firmware-manager \
     telnet
 
 systemctl disable pcscd.socket
