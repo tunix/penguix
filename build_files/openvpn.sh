a243a0b7510d462baa046dbf0e4219054c7196cd
Alper Kanat <me@alperkan.at>
2025-05-16 01:53:09 +0300

    openvpn additions

--- build_files/openvpn.sh
+++ build_files/openvpn.sh
@@ -14,4 +14,11 @@ dnf5 install -y \
     openvpn3-client \
     openvpn3-indicator
 
-systemctl enable openvpn-workaround.service
+# Apply fixes according to my comment at below GitHub issue
+# https://github.com/OpenVPN/openvpn3-linux/issues/229#issuecomment-2564890480
+
+systemctl enable openvpn-selinux-workaround.service
+systemctl enable openvpn-init-config.service
+
+# no need to check as the command seems to be idempotent
+setsebool -P dbus_access_tuntap_device=1
