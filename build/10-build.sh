8019433d352ec4a4bbf9557df177f230eea75cff
Alper Kanat <me@alperkan.at>
2026-05-12 01:08:33 +0300

    fix: remove masked service

--- build/10-build.sh
+++ build/10-build.sh
@@ -59,10 +59,9 @@ echo "::group:: System Configuration"
 
 # Enable/disable systemd services
 systemctl enable podman.socket
-systemctl disable systemd-remount-fs.service
-systemctl disable zfs-import-cache.service
-systemctl disable systemd-udev-settle.service
-systemctl disable NetworkManager-wait-online.service
+systemctl mask systemd-remount-fs.service
+systemctl mask zfs-import-cache.service
+systemctl mask systemd-udev-settle.service
 # Example: systemctl mask unwanted-service
 
 echo "::endgroup::"
